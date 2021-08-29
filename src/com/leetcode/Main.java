86f464eee0fea1b1911a50c327aa689bfdfda13e
Dustin George <>
2021-08-29 17:12:14 -0500

    first commit

---
+++ src/com/leetcode/Main.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+
+
+public class Main {
+    private static final ArraysAndStrings arraysAndStrings = new ArraysAndStrings();
+
+    public static void main(String[] args) {
+//        String word = "abc";
+//        boolean isUnique = arraysAndStrings.isUnique(word);
+//        System.out.println(isUnique);
+//        boolean isUnique2 = arraysAndStrings.isUniqueNoDataStructure(word);
+//        System.out.println(isUnique2);
+
+//        String word1 = "abcd";
+//        String word2 = "dcba";
+//        boolean isPerm = arraysAndStrings.isPermutation(word1, word2);
+//        System.out.println(isPerm);
+
+        //Skipped urlify:
+        //Solution iterate a character array if the element is a space
+        //replace the array with '%20' - not challenging
+
+        String word = "Tact Coa";
+        boolean isPalindromePerm = arraysAndStrings.isPalineDromePerm(word);
+        System.out.println(isPalindromePerm);
+
+    }
+
+
+}
+
+
+
